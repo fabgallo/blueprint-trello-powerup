d3d64f2abc2ef057a6e87eed767b3f4200e2bd0e
Fabio Gallo, Blueprint <fabio.gallo@blueprintsys.com>
2017-02-23 14:07:03 -0500

    try to bypass CORS #2b

--- js/login.js
+++ js/login.js
@@ -80,15 +80,15 @@ document.getElementById('login').addEventListener('click', function() {
         url: server + "authentication/v1/loginEx",
         dataType: 'json',
         async: false,
-        xhrFields: {
-            withCredentials: true
-        },
-        username: username,
-        password: password,
+        // xhrFields: {
+        //     withCredentials: true
+        // },
+        // username: username,
+        // password: password,
         //data: '{}',
-        //beforeSend: function (xhr) {
-        //    xhr.setRequestHeader('Authorization', make_base_auth(username, password));
-        //},
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', make_base_auth(username, password));
+        },
         success: function (data) {
             console.log(data);
         }
